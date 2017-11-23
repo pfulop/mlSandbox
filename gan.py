4aa6d3525240532b3967461df89f59d40564fb36
Pavol Fulop <pavol.fulop@regex.sk>
2017-11-23 15:10:40 +0100

    began

---
+++ gan.py
@@ -0,0 +1,35 @@
+import torchvision.models as models
+from torch import nn
+
+
+class D(nn.Module):
+
+    def convolution(self)
+        pass
+
+
+
+    def __init__(self, n_extra_layers):
+        super(DCGAND, self).__init__()
+
+
+    def forward(self, input):
+      pass
+
+
+class DCGANG(nn.Module):
+
+    def deconv(self):
+        main.add_module(f'{name}-{inf}.{of}.convt', nn.ConvTranspose2d(inf, of, a, b, c, bias=False))
+
+        pass
+
+    def upsampling(self):
+
+
+    def __init__(self):
+        super(DCGANG, self).__init__()
+
+
+    def forward(self, input):
+        pass
